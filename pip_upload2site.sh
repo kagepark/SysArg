fb971778e14bae8311fff8bff4b0983969bcee6f
Kage Park <kagepark1@gmail.com>
2021-12-03 12:44:44 -0800

    Update

--- pip_upload2site.sh
+++ pip_upload2site.sh
@@ -5,8 +5,16 @@
 #username =javatechy" > ~/.pypirc
 #python -m kmisc upload dist/*
 
+#######################
+# test.pypi.org
+#######################
 # Test upload
 #python3 -m twine upload --repository testpypi dist/*
 
 # Real upload
-python3 -m twine upload --repository-url https://test.pypi.org/legacy/ --repository legacy dist/*
+#python3 -m twine upload --repository-url https://test.pypi.org/legacy/ --repository legacy dist/*
+
+#######################
+# pypi.org
+#######################
+python3 -m twine upload --repository-url https://pypi.org/legacy/ --repository legacy dist/*
