3e4d3218812588f33d31518e1da17a18e40d761b
Kage Park <kagepark1@gmail.com>
2021-12-03 11:51:07 -0800

    Add pip script

---
+++ pip_upload2site.sh
@@ -0,0 +1,12 @@
+#echo "[distutils] 
+#index-servers=pypi
+#[pypi] 
+#repository = https://upload.pypi.org/legacy/ 
+#username =javatechy" > ~/.pypirc
+#python -m kmisc upload dist/*
+
+# Test upload
+#python3 -m twine upload --repository testpypi dist/*
+
+# Real upload
+python3 -m twine upload --repository-url https://test.pypi.org/legacy/ --repository legacy dist/*
